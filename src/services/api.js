c625d428668e63b013e14f9b7a667a28ca5cbb43
naveen93100 <naveen9310097505@gmail.com>
2026-02-23 12:25:43 +0530

    some changes

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const axiosInstance = axios.create({
-  baseURL: "https://gautamsolar.us",
+  baseURL: "https://gautamsolar.us", 
   // baseURL: "http://localhost:1008",
   withCredentials:true
 });
