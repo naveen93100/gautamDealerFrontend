095fbe64c61623dfbc776b88d4b3ebf12a7e5712
seraj0107 <it4@udamandi.com>
2026-08-08 11:04:07 +0530

    all complete

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -52,14 +52,10 @@
 
 // export default AppRoutes;
 
-
-
-
-
 import { useEffect } from "react";
 import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 
-import Login from "../pages/Login"; 
+import Login from "../pages/Login";
 import AdminLayout from "../pages/Dashboard/Layout/AdminLayout";
 
 import ProtectedRoute from "../components/common/ProtectedRoute";
@@ -86,7 +82,7 @@ const AppRoutes = () => {
     return (
         <Routes>
             <Route element={<PublicRoute />}>
-            {AuthRoutes}
+                {AuthRoutes}
                 <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
             </Route>
